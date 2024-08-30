71bff2360154c784077c973f11f8b1939f927076
Dominik Cech <dominik.cech@ibillboard.com>
2024-08-30 15:33:24 +0200

    refactoring

---
+++ caldav/oauth2.go
@@ -0,0 +1,127 @@
+package caldav
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"golang.org/x/oauth2"
+)
+
+var tokenCacheFile = "credentials.json"
+
+func NewOAuth2CalDAVService(serverUrl string, config *oauth2.Config) *CalDAVService {
+    token, err := retriveToken(tokenCacheFile, config)
+    if err != nil {
+        log.Fatalf("Cannot create OAuth2 CalDAVService because of: %v", err)
+    }
+
+	client := config.Client(context.Background(), token)
+
+	return NewCalDAVService(serverUrl, client)
+}
+
+func retriveToken(cacheFile string, config *oauth2.Config) (*oauth2.Token, error) {
+	token, fileErr := tokenFromFile(cacheFile)
+
+	if fileErr != nil {
+		token, webErr := getTokenFromWeb(config)
+		if webErr != nil {
+			return nil, fmt.Errorf("Unable to get token from web nor file: %v, %v", webErr, fileErr)
+		}
+		saveToken(cacheFile, token)
+	}
+
+	return token, nil
+}
+
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
+	codeChannel := make(chan string)
+	randState := "super-random-str"
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Query().Get("state") != randState {
+			http.Error(w, "Invalid state parameter", http.StatusBadRequest)
+			return
+		}
+
+		code := r.URL.Query().Get("code")
+		if code == "" {
+			http.Error(w, "Code not found", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "Authorization successful! You can close this window now.")
+		codeChannel <- code
+	})
+
+	server := &http.Server{Addr: ":8080"}
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Printf("HTTP server ListenAndServe: %v", err)
+		}
+	}()
+
+	authURL := config.AuthCodeURL(randState)
+
+    fmt.Println("Opening browser for authentication")
+	err := openBrowser(authURL)
+	if err != nil {
+        fmt.Printf("Unable to open browser automatically. Please open URL manually: %s\n", authURL)
+	}
+
+	// Wait for the code
+	code := <-codeChannel
+
+	// Shutdown the server
+	if err := server.Shutdown(context.Background()); err != nil {
+		log.Printf("HTTP server Shutdown: %v", err)
+	}
+
+	token, err := config.Exchange(context.TODO(), code, oauth2.AccessTypeOffline)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to exchange code for token: %v", err)
+	}
+
+	return token, nil
+}
+
+func openBrowser(url string) error {
+	var err error
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("Cannot open browser. Unsupported platform")
+	}
+	return err
+}
+
+func saveToken(path string, token *oauth2.Token) {
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
--- main.go
+++ main.go
@@ -1,171 +1,102 @@
 package main
 
 import (
-	"bytes"
-	"context"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
+	"net/url"
 	"os"
-	"os/exec"
-	"runtime"
+	"strings"
+	"time"
 
+	"github.com/docech/agenda/caldav"
 	"github.com/joho/godotenv"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
-type CalDAVClient struct {
-	ServerUrl string
-	Username  string
-	Password  string
-	client    *http.Client
-}
-
-func NewCalDAVClient(serverUrl string, config *oauth2.Config, token *oauth2.Token) *CalDAVClient {
-	client := config.Client(context.Background(), token)
-
-	return &CalDAVClient{
-		ServerUrl: serverUrl,
-		client:    client,
-	}
-}
-
-func (c *CalDAVClient) Request(method, path string, body []byte) ([]byte, error) {
-	url := fmt.Sprintf("%s%s", c.ServerUrl, path)
-
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(body))
-	if err != nil {
-		return nil, fmt.Errorf("Could not create request for url \"%s\" because: %v", url, err)
-	}
-
-	req.Header.Set("Content-Type", "application/xml; charset=utf-8")
-
-	resp, err := c.client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("Request to \"%s\" failed because: %v", url, err)
-	}
-	defer resp.Body.Close()
-
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("Unreadable response body: %v", err)
-	}
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("Server responded with non 200 status code: %d, %s", resp.StatusCode, string(respBody))
-	}
-
-	return respBody, nil
-}
-
-func getTokenFromWeb(config *oauth2.Config) (*oauth2.Token, error) {
-	codeChannel := make(chan string)
-	randState := "super-random-str"
-
-	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Query().Get("state") != randState {
-			http.Error(w, "Invalid state parameter", http.StatusBadRequest)
-			return
-		}
-
-		code := r.URL.Query().Get("code")
-		if code == "" {
-			http.Error(w, "Code not found", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Fprintf(w, "Authorization successful! You can close this window now.")
-		codeChannel <- code
-	})
-
-	server := &http.Server{Addr: ":8080"}
-	go func() {
-		if err := server.ListenAndServe(); err != http.ErrServerClosed {
-			log.Printf("HTTP server ListenAndServe: %v", err)
-		}
-	}()
-
-	authURL := config.AuthCodeURL(randState)
-	fmt.Printf("Please visit this URL to authorize the application: \n%s\n", authURL)
-
-	err := openBrowser(authURL)
-	if err != nil {
-		fmt.Println("Unable to open browser automatically. Please open the above URL manually.")
-	}
-
-	// Wait for the code
-	code := <-codeChannel
-
-	// Shutdown the server
-	ctx := context.Background()
-	if err := server.Shutdown(ctx); err != nil {
-		log.Printf("HTTP server Shutdown: %v", err)
-	}
-
-	token, err := config.Exchange(ctx, code)
-	if err != nil {
-		return nil, fmt.Errorf("unable to exchange code for token: %v", err)
-	}
-
-	return token, nil
-}
-
-func openBrowser(url string) error {
-	var err error
-	switch runtime.GOOS {
-	case "linux":
-		err = exec.Command("xdg-open", url).Start()
-	case "windows":
-		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	case "darwin":
-		err = exec.Command("open", url).Start()
-	default:
-		err = fmt.Errorf("unsupported platform")
-	}
-	return err
-}
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file. Make sure it exists")
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file. Make sure it exists")
+	}
 
-    clientID := os.Getenv("GOOGLE_CLIENT_ID")
-    clientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
-    calendarId := os.Getenv("GOOGLE_CALENDAR_ID")
-    calDAVServerUrl := fmt.Sprintf("https://apidata.googleusercontent.com/caldav/v2/%s", calendarId)
+	clientID := os.Getenv("GOOGLE_CLIENT_ID")
+	clientSecret := os.Getenv("GOOGLE_CLIENT_SECRET")
+	calendarId := os.Getenv("GOOGLE_CALENDAR_ID")
+	calDAVServerUrl := fmt.Sprintf("https://apidata.googleusercontent.com/caldav/v2/%s", url.QueryEscape(calendarId))
 
 	config := &oauth2.Config{
 		ClientID:     clientID,
 		ClientSecret: clientSecret,
 		RedirectURL:  "http://localhost:8080/callback",
-		Scopes:       []string{"https://www.googleapis.com/auth/calendar.readonly"},
+		Scopes:       []string{"https://www.googleapis.com/auth/calendar"},
 		Endpoint:     google.Endpoint,
 	}
 
-	token, err := getTokenFromWeb(config)
-	if err != nil {
-		log.Fatalf("Unable to get token from web: %v", err)
-	}
+	service := caldav.NewOAuth2CalDAVService(calDAVServerUrl, config)
 
-    client := NewCalDAVClient(calDAVServerUrl, config, token)
+// 	now := time.Now()
+// 	nowPlus7Days := now.AddDate(0, 0, 7)
+//
+//     //Gets calendar events, REPORT, has to be sent to calendar endpoint
+// 	calendarEvents := `<?xml version="1.0" encoding="utf-8" ?>
+// <c:calendar-query xmlns:d="DAV:" xmlns:c="urn:ietf:params:xml:ns:caldav">
+//     <d:prop>
+//         <d:getetag />
+//         <c:calendar-data />
+//     </d:prop>
+//     <c:filter>
+//         <c:comp-filter name="VCALENDAR">
+//             <c:comp-filter name="VEVENT">
+//                 <c:time-range start="START_DATE" end="END_DATE"/>
+//             </c:comp-filter>
+//         </c:comp-filter>
+//     </c:filter>
+// </c:calendar-query>`
+// 	calendarEvents = strings.ReplaceAll(calendarEvents, "START_DATE", now.Format("20060102T150405Z"))
+// 	calendarEvents = strings.ReplaceAll(calendarEvents, "END_DATE", nowPlus7Days.Format("20060102T150405Z"))
 
-    body := `<?xml version="1.0" encoding="utf-8" ?>
-    <D:propfind xmlns:D="DAV:">
-        <D:prop>
-            <D:resourcetype/>
-            <D:displayname/>
-        </D:prop>
-    </D:propfind>`
+// 	listCalendars := `<?xml version="1.0" encoding="UTF-8" ?>
+// <D:propfind xmlns:D="DAV:">
+//   <D:prop>
+//     <D:displayname />
+//     <C:calendar-home-set xmlns:C="urn:ietf:params:xml:ns:caldav" />
+//   </D:prop>
+// </D:propfind>`
 
-    response, err := client.Request("PROPFIND", "/events", []byte(body))
+//     discovery := `<d:propfind xmlns:d="DAV:" xmlns:cs="http://calendarserver.org/ns/" xmlns:c="urn:ietf:params:xml:ns:caldav">
+//   <d:prop>
+//      <d:resourcetype />
+//      <d:displayname />
+//      <cs:getctag />
+//      <c:supported-calendar-component-set />
+//   </d:prop>
+// </d:propfind>`
+//
+//     body := discovery
+//     method := "DISCOVERY"
+//
+//     var path string
+//     if method == "REPORT" {
+//         path =  "/events"
+//     } else if method == "PROPFIND"{
+//         path = "/user"
+//     } else {
+//         method = "PROPFIND"
+//         path = "/"
+//     }
+        
+    req, err := service.NewUserPrincipalRequest()
     if err != nil {
-        fmt.Printf("Error: %v\n", err)
-        return
+        log.Fatalf("Could not create request: %v", err)
     }
 
-    fmt.Printf("Response: %s\n", string(response))
+    response, err := service.Do(req)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Response: %s\n", string(response))
 }
