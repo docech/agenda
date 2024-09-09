340c4a17d96f078ebcc75539ffb3f90bd739007c
Dominik Cech <dominik.cech@ibillboard.com>
2024-09-09 15:23:41 +0200

    styling event list

--- eventlist/model.go
+++ eventlist/model.go
@@ -1,19 +1,22 @@
 package eventlist
 
 import (
+	"slices"
 	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
-) 
+)
 
 var (
-    groupTextWidth = 20
+	groupTextWidth = 20
 
-    groupTextStyle = lipgloss.NewStyle().Width(groupTextWidth)
-    whenTextStyle= lipgloss.NewStyle().Width(10)
-    summaryTextStyle = lipgloss.NewStyle().PaddingLeft(5)
-    itemStyle = lipgloss.NewStyle().PaddingLeft(groupTextWidth)
+    groupLineStyle   = lipgloss.NewStyle().BorderBottom(true).BorderStyle(lipgloss.ThickBorder())
+	groupTextStyle   = lipgloss.NewStyle().Width(groupTextWidth)
+	whenTextStyle    = lipgloss.NewStyle().Width(10)
+	summaryTextStyle = lipgloss.NewStyle().PaddingLeft(5)
+	itemStyle        = lipgloss.NewStyle().PaddingLeft(groupTextWidth)
+    selectedItemStyle= lipgloss.NewStyle().Bold(true)
 )
 
 type timeDefinition interface {
@@ -47,7 +50,8 @@ type group struct {
 }
 
 type model struct {
-	groups []group
+	groups       []group
+	selectedItem int
 }
 
 func New() model {
@@ -69,6 +73,7 @@ func New() model {
 				},
 			},
 		},
+		selectedItem: -1,
 	}
 }
 
@@ -79,8 +84,19 @@ func (m model) Init() tea.Cmd {
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if msg.Type == tea.KeyCtrlC {
+		switch msg.String() {
+		case "ctrl+c":
 			return m, tea.Quit
+		case "j":
+			newSelectedItem := m.selectedItem + 1
+			if newSelectedItem < m.itemsCount() {
+				m.selectedItem = newSelectedItem
+			}
+		case "k":
+			newSelectedItem := m.selectedItem - 1
+			if newSelectedItem >= 0 {
+				m.selectedItem = newSelectedItem
+			}
 		}
 	}
 
@@ -88,27 +104,49 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	str := ""
+    gIdxAdjustment := 0
+
+    groupsCmp := make([]string, len(m.groups)) 
 	for _, g := range m.groups {
-		if len(g.items) == 0 {
-			continue
+        var itemsCmp []string 
+		for idx, i := range g.items {
+			itemCmp := lipgloss.JoinHorizontal(lipgloss.Left,
+				whenTextStyle.Render(i.When.toText()),
+				summaryTextStyle.Render(i.Summary),
+			)
+            if m.selectedItem == gIdxAdjustment + idx {
+                itemCmp = selectedItemStyle.Render(itemCmp)
+            }
+            if idx != 0 {
+                itemCmp = itemStyle.Render(itemCmp)
+            }
+
+            itemsCmp = append(itemsCmp, itemCmp)
 		}
 
-		firstItem := g.items[0]
+        firstItemCmp, itemsCmp := itemsCmp[0], itemsCmp[1:]
 
-		str += lipgloss.JoinHorizontal(lipgloss.Left,
-            groupTextStyle.Render(g.Title + " " + g.Description),
-            whenTextStyle.Render(firstItem.When.toText()),
-            summaryTextStyle.Render(firstItem.Summary),
-        ) + "\n" 
-
-		for _, i := range g.items[1:] {
-			str += itemStyle.Render(lipgloss.JoinHorizontal(lipgloss.Left,
-                whenTextStyle.Render(i.When.toText()),
-                summaryTextStyle.Render(i.Summary),
-            )) + "\n"
-		}
+		groupHeadingCmp := lipgloss.JoinHorizontal(lipgloss.Left,
+			groupTextStyle.Render(g.Title+" "+g.Description),
+			firstItemCmp,
+		)
+        groupCmp := groupLineStyle.Render(
+            lipgloss.JoinVertical(
+                lipgloss.Left, 
+                slices.Insert(itemsCmp, 0, groupHeadingCmp)...,
+            ),
+        )
+		groupsCmp = append(groupsCmp, groupCmp)
+        gIdxAdjustment += len(g.items)
 	}
 
-	return str
+    return lipgloss.JoinVertical(lipgloss.Left, groupsCmp...)
+}
+
+func (m model) itemsCount() int {
+	count := 0
+	for _, g := range m.groups {
+		count += len(g.items)
+	}
+	return count
 }
